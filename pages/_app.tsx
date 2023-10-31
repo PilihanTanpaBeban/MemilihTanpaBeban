5e9644ded2d0acbba2ade96194fefd00884dd73a
AuserZ <47166961+AuserZ@users.noreply.github.com>
2023-10-31 17:23:05 +0700

    change icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: any) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="../assets/images/Favicon.png" />
       </Head>
       <Layout>
         <Component {...pageProps} />
