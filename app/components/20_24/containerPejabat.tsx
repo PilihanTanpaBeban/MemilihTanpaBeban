855d1a1068be676fcfcbec58786a252e62d6265e
NewbieChiii <sachioshaan@yahoo.co.id>
2023-12-14 04:16:06 +0700

    update autoslide on containerPejabat

--- app/components/20_24/containerPejabat.tsx
+++ app/components/20_24/containerPejabat.tsx
@@ -1,7 +1,10 @@
 import { Collapse, Flex, ScrollArea, rem } from "@mantine/core";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import BoxPejabat from "./itemPejabat";
 import { useDisclosure } from "@mantine/hooks";
+import { Carousel } from "@mantine/carousel";
+import "@mantine/carousel/styles.css";
+import Autoplay from 'embla-carousel-autoplay';
 
 interface containerPejabatProps {
   data: any;
@@ -12,10 +15,18 @@ const ContainerPejabat: React.FC<containerPejabatProps> = ({
   data,
   isVertical,
 }) => {
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
+
   const dataPejabat = data.map((item: any) => (
     <React.Fragment key={item.id}>
       <BoxPejabat data={item} />
     </React.Fragment>
+  ));
+
+  const caroselPejabat = data.map((item: any) => (
+    <Carousel.Slide key={item.id} pt={10} pb={10}>
+      <BoxPejabat data={item} />
+    </Carousel.Slide>
   ));
   const [opened, { toggle }] = useDisclosure(true);
 
@@ -27,12 +38,24 @@ const ContainerPejabat: React.FC<containerPejabatProps> = ({
             {dataPejabat}
           </Flex>
         </Collapse>
-      ) : (
-        <ScrollArea w="100%">
-          <Flex direction="row" gap="md" p={rem(20)}>
-            {dataPejabat}
-          </Flex>
-        </ScrollArea>
+      ) : ( 
+        <Carousel
+          orientation={"horizontal"}
+          slideGap={"md"}
+          slideSize={"10%"}
+          align="start"
+          plugins={[autoplay.current]}
+          onMouseEnter={autoplay.current.stop}
+          onMouseLeave={autoplay.current.reset}
+          loop
+        >
+            {caroselPejabat}
+        </Carousel>
+        // <ScrollArea w="100%">
+        //   <Flex direction="row" gap="md" p={rem(20)}>
+        //     {dataPejabat}
+        //   </Flex>
+        // </ScrollArea>
       )}
     </>
   );
