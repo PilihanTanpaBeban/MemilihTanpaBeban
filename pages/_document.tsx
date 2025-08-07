b5e0e7c3603ba7a5c8975cb48092e55a6bffdd33
AuserZ <mzainurroziqinakbar@gmail.com>
2025-08-07 22:32:22 +0700

    add meta on head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,12 +15,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
           rel="stylesheet"
         />
+        <meta name="description" content="MemilihTanpaBeban adalah platform yang dirancang untuk membantu Anda membuat keputusan yang terinformasi tanpa bias. Jelajahi berbagai topik dan dapatkan wawasan." />
+        <meta name="keywords" content="pemilu, tanpa beban, pilih, pilihan, bebas, pilkada, pilpres, anggota dewan, memilih tanpa beban, partai, partai politik, pemilu 2024" />
       </Head>
       <body>
         <Main />
