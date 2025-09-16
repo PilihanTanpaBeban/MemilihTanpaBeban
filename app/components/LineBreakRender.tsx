f39de7c70d11147dc9552e2e99300bb31cc2e582
AuserZ <mzainurroziqinakbar@gmail.com>
2025-09-16 20:20:38 +0700

    add line breakrender

--- app/components/LineBreakRender.tsx
+++ app/components/LineBreakRender.tsx
@@ -1,10 +1,15 @@
 import { List, ListItem, rem, ThemeIcon, Text } from "@mantine/core";
 import React from "react";
 import { primaryColor } from "../../public/colors";
-import { IconCircle, IconCircleCheck, IconCircleDashed, IconCircleFilled } from "@tabler/icons-react";
+import {
+  IconCircle,
+  IconCircleCheck,
+  IconCircleDashed,
+  IconCircleFilled,
+} from "@tabler/icons-react";
 import classes from "../components/styles/linebreak.module.css";
-import sanitizeHtml from 'sanitize-html';
-
+import sanitizeHtml from "sanitize-html";
+import DOMPurify from "dompurify";
 
 export const renderTextWithLineBreaks = (text: String) => {
   const lines = text.split("\n");
@@ -27,13 +32,12 @@ export const renderTextWithLineBreaksNoSpaces = (text: String) => {
   ));
 };
 
-
 const sanitizeHTML = (html: string) => {
   return sanitizeHtml(html, {
-    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'br', 'ul', 'li'],
+    allowedTags: ["b", "i", "em", "strong", "a", "br", "ul", "li"],
     allowedAttributes: {
-      'a': ['href', 'style']
-    }
+      a: ["href", "style"],
+    },
   });
 };
 
@@ -41,24 +45,45 @@ export const renderTextWithLineBreaksList = (text: String) => {
   const lines = text.split("\n");
   return (
     <List
-      icon={<IconCircleFilled style={{ color: '#966fa2', borderRadius: '50%', width: rem(7), height: rem(7) }} />}
+      icon={
+        <IconCircleFilled
+          style={{
+            color: "#966fa2",
+            borderRadius: "50%",
+            width: rem(7),
+            height: rem(7),
+          }}
+        />
+      }
       classNames={{
-        item: classes['mantine-List-item']
+        item: classes["mantine-List-item"],
       }}
     >
-      {
-        lines.map((line, index) => (
-          <React.Fragment key={index}>
-            <Text dangerouslySetInnerHTML={{ __html: sanitizeHTML(line) }} />
-          </React.Fragment>
-        ))
-      }
+      {lines.map((line, index) => (
+        <React.Fragment key={index}>
+          <Text dangerouslySetInnerHTML={{ __html: sanitizeHTML(line) }} />
+        </React.Fragment>
+      ))}
     </List>
   );
 };
 
+// enhance links by forcing target="_blank"
+const addTargetBlank = (html: string) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+
+  doc.querySelectorAll("a").forEach((a) => {
+    a.setAttribute("target", "_blank");
+    a.setAttribute("rel", "noopener noreferrer"); // security best practice
+  });
+
+  return doc.body.innerHTML;
+};
+
 export const renderTextWithHtml = (text: string) => {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: sanitizeHTML(text) }} />
-  );
-}
+  const safeHtml = sanitizeHTML(text);
+  const enhancedHtml = addTargetBlank(safeHtml);
+
+  return <div dangerouslySetInnerHTML={{ __html: enhancedHtml }} />;
+};
